bb79dad993d3ebd145544ed20c5924664ca7cc68
mrjohnming <john.ngai@hotmail.com>
2022-02-21 15:11:47 -0500

    Replace hardcoded values with data from props

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,12 +1,11 @@
-function Player() {
+function Player(props) {
+  const { gamerTag, firstName, lastName, wins } = props;
   return (
     <article className="Player">
-      <h1>
-        John <span>Secret</span> Doe
-      </h1>
-      <h2 className="zero">Current with no wins :(</h2>
-      <h2>Current at 1 win</h2>
-      <h2>Current at 1+ wins</h2>
+      <h1>{firstName} <span>{gamerTag}</span> {lastName}</h1>
+      {wins === 0 && <h2 className="zero">Current with no wins :(</h2>}
+      {wins === 1 && <h2>Current at 1 win</h2>}
+      {wins > 1 && <h2>Current at 1+ wins</h2>}
     </article>
   );
 }
