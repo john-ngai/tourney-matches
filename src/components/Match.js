db3e55cfa202a7cf17272c4e99cc74cee5af3854
mrjohnming <john.ngai@hotmail.com>
2022-02-20 22:05:27 -0500

    Create the Match & MatchList components

---
+++ src/components/Match.js
@@ -0,0 +1,13 @@
+function Match() {
+  return (
+    <article className="Match">
+      <h1>
+        Disguised Lizard <span>vs</span> DrSpiteful
+      </h1>
+      <h2>Disguised Lizard is the winner by 1!</h2>
+      <h2>No winners yet!</h2>
+    </article>
+  );
+}
+
+export default Match;
---
+++ src/components/MatchList.js
@@ -0,0 +1,12 @@
+import Match from 'Match.js';
+
+function MatchList() {
+  return (
+    <section className="PlayerList MatchList">
+      <h1>Match List</h1>
+      <Match />
+    </section>
+  );
+}
+
+export default MatchList;
