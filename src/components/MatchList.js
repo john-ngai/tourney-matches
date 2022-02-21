0dae4e5e2e592648c232f4ca13d8a6dce773e817
mrjohnming <john.ngai@hotmail.com>
2022-02-21 15:05:34 -0500

    Render a single match

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -2,10 +2,11 @@ import Match from './Match';
 import matchData from '../data/matchData';
 
 function MatchList() {
+  const oneMatch = matchData[0];
   return (
     <section className="PlayerList MatchList">
       <h1>Match List</h1>
-      <Match />
+      <Match {...oneMatch} />
     </section>
   );
 }
