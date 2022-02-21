5d8ff0882ce8b7800971ab7e860dfa93b9610cb3
mrjohnming <john.ngai@hotmail.com>
2022-02-20 21:55:41 -0500

    Create the Player & PlayerList components

---
+++ src/components/Player.js
@@ -0,0 +1,14 @@
+function Player() {
+  return (
+    <article className="Player">
+      <h1>
+        John <span>Secret</span> Doe
+      </h1>
+      <h2 className="zero">Current with no wins :(</h2>
+      <h2>Current at 1 win</h2>
+      <h2>Current at 1+ wins</h2>
+    </article>
+  );
+}
+
+export default Player;
